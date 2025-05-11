1f6e355b8cb861ee2993b0bdac3874cc1785a3dc
Nutan-Kum12 <166277808+Nutan-Kum12@users.noreply.github.com>
2025-05-11 22:24:32 +0530

    Testing

--- app/main.py
+++ app/main.py
@@ -5,5 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def home():
+    return {"message": "🚀 FastAPI Project from nwnfwk;lwkp CMD is working!"}
+@app.get("/home")
+def home2():
     return {"message": "🚀 FastAPI Project from CMD is working!"}
 
